58f810445ca8879e14447acb6a797edf441d6cd2
jr <jr@192.168.1.4>
2019-10-16 06:11:56 -0700

    Remove comment

--- Circle/src/main/java/com/web/circle/CircleApplication.java
+++ Circle/src/main/java/com/web/circle/CircleApplication.java
@@ -44,11 +44,7 @@ public class CircleApplication {
         return tomcat;
     }
 
-    /*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     */
+
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
