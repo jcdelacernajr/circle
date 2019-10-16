63d05de5e3f60b691972b0ccd790010a7891813d
jr <jr@192.168.1.4>
2019-10-16 06:10:40 -0700

    Added author header

--- Circle/src/main/java/com/web/circle/CircleApplication.java
+++ Circle/src/main/java/com/web/circle/CircleApplication.java
@@ -11,6 +11,10 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * 
+ * @author jr
+ * */
 @SpringBootApplication
 @EnableJpaAuditing // Enabling JPA Auditing
 public class CircleApplication {
