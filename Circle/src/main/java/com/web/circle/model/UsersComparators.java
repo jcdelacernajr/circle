23766865fd3481cfd1ccdec4308761fc7358d329
jr <jr@192.168.1.9>
2020-04-09 08:34:52 -0700

    Refactoring fragments files

--- Circle/src/main/java/com/web/circle/model/UsersComparators.java
+++ Circle/src/main/java/com/web/circle/model/UsersComparators.java
@@ -3,14 +3,10 @@ package com.web.circle.model;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-
 import com.web.circle.model.tablepaging.Direction;
-
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
-
 
 public final class UsersComparators {
 	
--- Circle/src/main/java/com/web/circle/service/UserTableServiceImplementation.java
+++ Circle/src/main/java/com/web/circle/service/UserTableServiceImplementation.java
@@ -45,104 +45,103 @@ public class UserTableServiceImplementation {
     	this.userRepository = userRepository;
 	}
 
-		public Page<UsersModel> getUsers(PagingRequest pagingRequest) {
-	        ObjectMapper objectMapper = new ObjectMapper();
+	public Page<UsersModel> getUsers(PagingRequest pagingRequest) {
+        ObjectMapper objectMapper = new ObjectMapper();
 
-	        try {
-	        	// Get the json value from json file. For testing purposes.
-	            //List<UsersModel> users = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("users.json"), 
-	            //        new TypeReference<List<UsersModel>>() {
-                //});
-	        	
-	        	
-	        	JSONArray arr  = new JSONArray();
-	            List<Users> uEntity = userRepository.findAll();
-	            for (Users e : uEntity) {
-	            	JSONObject jo = new JSONObject();
-	            	jo.put("userId", e.getUserId());
-	            	jo.put("organizationFk", e.getOrganizationFk());
-	            	jo.put("email", e.getEmail());
-	            	jo.put("username", e.getUsername());
-	            	jo.put("password", "****");
-	            	jo.put("active", e.getIsActive());
-	            	jo.put("accountNonExpired", e.isAccountNonExpired());
-	            	jo.put("accountNonLocked", e.isAccountNonLocked());
-	            	jo.put("credentialsNonExpired", e.isCredentialsNonExpired());
-	            	arr.put(jo);
-				}
-	            JsonParser jParser = new JsonParser();
-	            JsonElement jElement = jParser.parse(arr.toString());
-	            // System.out.println("arr: " +  jElement);
-	            List<UsersModel> users =  objectMapper.readValue(jElement.toString(),  new TypeReference<List<UsersModel>>() { });
-	            return getPage(users, pagingRequest);
+        try {
+        	// Get the json value from json file. For testing purposes.
+            //List<UsersModel> users = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("users.json"), 
+            //        new TypeReference<List<UsersModel>>() {
+            //});
+        	
+        	
+        	JSONArray arr  = new JSONArray();
+            List<Users> uEntity = userRepository.findAll();
+            for (Users e : uEntity) {
+            	JSONObject jo = new JSONObject();
+            	jo.put("userId", e.getUserId());
+            	jo.put("organizationFk", e.getOrganizationFk());
+            	jo.put("email", e.getEmail());
+            	jo.put("username", e.getUsername());
+            	jo.put("password", "****");
+            	jo.put("active", e.getIsActive());
+            	jo.put("accountNonExpired", e.isAccountNonExpired());
+            	jo.put("accountNonLocked", e.isAccountNonLocked());
+            	jo.put("credentialsNonExpired", e.isCredentialsNonExpired());
+            	arr.put(jo);
+			}
+            JsonParser jParser = new JsonParser();
+            JsonElement jElement = jParser.parse(arr.toString());
+            // System.out.println("arr: " +  jElement);
+            List<UsersModel> users =  objectMapper.readValue(jElement.toString(),  new TypeReference<List<UsersModel>>() { });
+            return getPage(users, pagingRequest);
 
-	        } catch (Exception e) {
-	        	log.error(e.getMessage(), e);
-	        }
+        } catch (Exception e) {
+        	log.error(e.getMessage(), e);
+        }
 
-	        return new Page<>();
-	    }
+        return new Page<>();
+    }
 
-	    private Page<UsersModel> getPage(List<UsersModel> users, PagingRequest pagingRequest) {
-	        List<UsersModel> filtered = users.stream().sorted(sortUsers(pagingRequest))
-	                                           .filter(filterUsers(pagingRequest))
-	                                           .skip(pagingRequest.getStart())
-	                                           .limit(pagingRequest.getLength())
-	                                           .collect(Collectors.toList());
+    private Page<UsersModel> getPage(List<UsersModel> users, PagingRequest pagingRequest) {
+        List<UsersModel> filtered = users.stream().sorted(sortUsers(pagingRequest))
+                                           .filter(filterUsers(pagingRequest))
+                                           .skip(pagingRequest.getStart())
+                                           .limit(pagingRequest.getLength())
+                                           .collect(Collectors.toList());
 
-	        long count = users.stream().filter(filterUsers(pagingRequest)).count();
+        long count = users.stream().filter(filterUsers(pagingRequest)).count();
 
-	        Page<UsersModel> page = new Page<>(filtered);
-	        page.setRecordsFiltered((int) count);
-	        page.setRecordsTotal((int) count);
-	        page.setDraw(pagingRequest.getDraw());
+        Page<UsersModel> page = new Page<>(filtered);
+        page.setRecordsFiltered((int) count);
+        page.setRecordsTotal((int) count);
+        page.setDraw(pagingRequest.getDraw());
 
-	        return page;
-	    }
+        return page;
+    }
 
-	    /**
-	     * Table search function
-	     * 
-	     * @param PagingRequest
-	     * @return Return value must not a null value.
-	     * @throws If the user table contains any null value. this function throws a null pointer exception.
-	     * */
-	    private Predicate<UsersModel> filterUsers(PagingRequest pagingRequest) {
-	        if (pagingRequest.getSearch() == null || StringUtils.isEmpty(pagingRequest.getSearch().getValue())) {
-	            return user -> true;
-	        }
+    /**
+     * Table search function
+     * 
+     * @param PagingRequest
+     * @return Return value must not a null value.
+     * @throws If the user table contains any null value. this function throws a null pointer exception.
+     * */
+    private Predicate<UsersModel> filterUsers(PagingRequest pagingRequest) {
+        if (pagingRequest.getSearch() == null || StringUtils.isEmpty(pagingRequest.getSearch().getValue())) {
+            return user -> true;
+        }
 
-	        String value = pagingRequest.getSearch().getValue();
-	        
-	        //log.info("Value: "+ value);
-	        
-	        return user -> user.getEmail().toLowerCase().contains(value) || user.getUsername().toLowerCase().contains(value) || 
-	        		String.valueOf(user.getUserId()).toLowerCase().contains(value);
-	    }
+        String value = pagingRequest.getSearch().getValue();
+        
+        //log.info("Value: "+ value);
+        
+        return user -> user.getEmail().toLowerCase().contains(value) || user.getUsername().toLowerCase().contains(value) || 
+        		String.valueOf(user.getUserId()).toLowerCase().contains(value);
+    }
 
-	    private Comparator<UsersModel> sortUsers(PagingRequest pagingRequest) {
-	        if (pagingRequest.getOrder() == null) {
-	            return EMPTY_COMPARATOR;
-	        }
+    private Comparator<UsersModel> sortUsers(PagingRequest pagingRequest) {
+        if (pagingRequest.getOrder() == null) {
+            return EMPTY_COMPARATOR;
+        }
 
-	        try {
-	            Order order = pagingRequest.getOrder().get(0);
+        try {
+            Order order = pagingRequest.getOrder().get(0);
 
-	            int columnIndex = order.getColumn();
-	            Column column = pagingRequest.getColumns().get(columnIndex);
+            int columnIndex = order.getColumn();
+            Column column = pagingRequest.getColumns().get(columnIndex);
 
-	            Comparator<UsersModel> comparator = UsersComparators.getComparator(column.getData(), order.getDir());
-	            if (comparator == null) {
-	                return EMPTY_COMPARATOR;
-	            }
+            Comparator<UsersModel> comparator = UsersComparators.getComparator(column.getData(), order.getDir());
+            if (comparator == null) {
+                return EMPTY_COMPARATOR;
+            }
 
-	            return comparator;
+            return comparator;
 
-	        } catch (Exception e) {
-	            log.error(e.getMessage(), e);
-	        }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
 
-	        return EMPTY_COMPARATOR;
-	    }
-
+        return EMPTY_COMPARATOR;
+    }
 }
